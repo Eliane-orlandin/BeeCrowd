efdce200264c471de4bfa15cdcf45ab4c22c9315
Eliane Orlandin <liorlandin33@gmail.com>
2024-06-01 14:01:04 -0300

    Resolução exercício 13 - Área

---
+++ area.js
@@ -0,0 +1,32 @@
+/*
+Escreva um programa que leia três valores com ponto flutuante de dupla precisão: A, B e C. Em seguida, calcule e mostre:
+a) a área do triângulo retângulo que tem A por base e C por altura.
+b) a área do círculo de raio C. (pi = 3.14159)
+c) a área do trapézio que tem A e B por bases e C por altura.
+d) a área do quadrado que tem lado B.
+e) a área do retângulo que tem lados A e B.
+Entrada
+O arquivo de entrada contém três valores com um dígito após o ponto decimal.
+Saída
+O arquivo de saída deverá conter 5 linhas de dados. Cada linha corresponde a uma das áreas descritas acima, sempre com mensagem correspondente e um espaço entre os dois pontos e o valor. O valor calculado deve ser apresentado com 3 dígitos após o ponto decimal.
+*/
+
+var input = require("fs").readFileSync("/dev/stdin", "utf8");
+var lines = input.split("\n");
+
+var valores = lines[0].split(" ");
+var A = parseFloat(valores[0]);
+var B = parseFloat(valores[1]);
+var C = parseFloat(valores[2]);
+
+var triangulo = (A * C) / 2;
+var circulo = 3.14159 * C * C;
+var trapezio = ((A + B) * C) / 2;
+var quadrado = B * B;
+var retangulo = A * B;
+
+console.log("TRIANGULO: " + triangulo.toFixed(3));
+console.log("CIRCULO: " + circulo.toFixed(3));
+console.log("TRAPEZIO: " + trapezio.toFixed(3));
+console.log("QUADRADO: " + quadrado.toFixed(3));
+console.log("RETANGULO: " + retangulo.toFixed(3));
